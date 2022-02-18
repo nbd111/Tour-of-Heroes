dd648b96e430a12d738ff753cccc1021016ec566
Nick DeLozier <nickdelozier@gmail.com>
2022-02-18 14:46:10 -0500

    Add Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,28 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../models/hero';
-import { HEROES } from '../models/mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
   selectedHero?: Hero;
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
- 
 
 // The ngOnInit() is a lifecycle hook. Angular calls ngOnInit() shortly after creating a component. 
 //It's a good place to put initialization logic.
